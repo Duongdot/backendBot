d7a7e2d375fe470cf8656d6a34bc46ddcdec562a
Duongdot <babyduong29@gmail.com>
2023-04-28 01:16:34 +0700

    Initial server commit

---
+++ src/routes/api.js
@@ -0,0 +1,14 @@
+import express from "express";
+import userController from "../controllers/userController";
+
+let router = express.Router();
+
+let initAPIRoutes = (app) => {
+  router.post(
+    "/enrollment-application",
+    userController.handleEnrollmentApplication
+  );
+  return app.use("/api/v1/", router);
+};
+
+module.exports = initAPIRoutes;
